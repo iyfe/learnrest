d78e6a969f5ecd2fa4b3905f2870d87565594e68
Ifunanya Ukachukwu <icukachuk@gmail.com>
2017-01-14 17:24:38 -0500

    Tutorial from http://bigspaceship.github.io/blog/2014/05/14/how-to-create-a-rest-api-with-node-dot-js7

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3001);
-console.log('Server running at http://localhost:3001/');
+var express = require('express'),
+mongoose = require('mongoose'),
+fs = require('fs');
+
+var mongoUri = 'mongodb://localhost/LearnREST';
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + mongoUri);
+});
+
+var app = express();
+//var bodyParser = require('body-parser')
+
+
+ // app.use(express.bodyParser());
+
+require('./models/musician');
+require('./routes')(app);
+
+app.listen(3001);
+console.log('Listening on port 3001...');
